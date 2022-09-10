a8c3b64b6b938b8dbe72ab1754cc685fe33e5fd2
Tatsukawa <takun.cloud961@gmail.com>
2022-09-11 00:06:35 +0900

    ヘッダー作成

--- src/router/Router.tsx
+++ src/router/Router.tsx
@@ -2,6 +2,7 @@ import { FC, memo } from "react";
 import { Route, Routes } from "react-router-dom";
 import { Login } from "../components/pages/Login";
 import { Page404 } from "../components/pages/Page404";
+import { HeaderLayout } from "../components/templates/HeaderLayout";
 import { homeRoutes } from "./HomeRoutes";
 
 export const Router: FC = memo(() => {
@@ -10,7 +11,12 @@ export const Router: FC = memo(() => {
       <Route path="/" element={<Login />} />
       <Route path="home">
         {homeRoutes.map((route) => (
-          <Route key={route.path} index={route.index} path={route.path} element={route.element} />
+          <Route
+            key={route.path}
+            index={route.index}
+            path={route.path}
+            element={<HeaderLayout>{route.element}</HeaderLayout>}
+          />
         ))}
       </Route>
       <Route path="*" element={<Page404 />} />
